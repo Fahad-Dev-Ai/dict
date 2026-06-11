9b381e54d61d2bf5423b17b02d2ea6cdffc8bebe
Fahad-Dev-Ai <fahadshoaib7777@gmail.com>
2026-06-12 01:51:25 +0500

    Delete read and write directory

--- read and write/generators.py
+++
@@ -1,9 +0,0 @@
-def get_nums():
-    yield 1
-    yield 2
-    yield 3
-    yield 4
-    
-gen = get_nums()
-for i in gen:
-    print (i)    
--- read and write/list_comprehensions.py
+++
@@ -1,2 +0,0 @@
-numbers = [i for i in range(10) if i%2==0]
-print(numbers)
